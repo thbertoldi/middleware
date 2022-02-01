ce380f3665c0ec333f5c1d06da805f277b732804
Thiago H. Bertoldi <thb96@hotmail.com>
2022-01-31 23:16:48 -0300

    Use bootstrap template
    Signed-off-by: Thiago H. Bertoldi <thb96@hotmail.com>

---
+++ web/src/App/App.svelte
@@ -0,0 +1,20 @@
+<script>
+ import Config from '../Components/Config/Config.svelte';
+ import { onMount } from 'svelte';
+ onMount(async () => {
+     console.log(location.href + "api/config");
+     fetch(location.href + "api/config")
+         .then(response => response.json())
+         .then(data => {
+		         console.log(data);
+             apiData.set(data);
+         }).catch(error => {
+             console.log(error);
+             return [];
+         });
+ });
+</script>
+
+<h1 class="bg-primary text-light text-center">IoT Middleware</h1>
+
+<Config config={"b"}></Config>
--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,14 @@
-import App from "./App.svelte";
+import 'bootstrap/scss/bootstrap.scss';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+import App from './App/App.svelte';
 
 const app = new App({
-  target: document.getElementById("app"),
+	target: document.body,
+	props: {
+		name: 'world'
+	}
 });
+
+window.app = app;
 
 export default app;
